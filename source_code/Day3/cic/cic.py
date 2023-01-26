add8eee558b2ccaf6b318528b0b586b92586e948
Lisa Zacarias <zacarias@slac.stanford.edu>
2023-01-25 22:32:45 -0600

    updated CIC code

--- source_code/Day3/cic/cic.py
+++ source_code/Day3/cic/cic.py
@@ -1,6 +1,5 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from numpy import exp
 from scipy.signal import freqz
 
 # wave_samp_per is the total decimation factor
@@ -14,15 +13,37 @@ fs = 115e6  # Hz
 
 def main():
     fig, axs = plt.subplots()
-    b = [1, -2 * exp(R), exp(R)]
+    """
+    B polynomial
+    
+    (1 - z^(-RM))^N
+    (1 - z^(-R))^2
+    (1 - z^(-R)) * (1 - z^(-R))
+    1 - z^(-R) - z^(-R) + z^(-2R)
+    1 - 2z^(-R) + z^(-2R)
+    R = 115 -> 1 - 2z^(-115) + z^(-230)
+    """
+    b = [0 for _ in range(2 * R + 1)]
+    b[0] = 1
+    b[R] = -2
+    b[2 * R] = 1
+    print(b)
+    
+    """
+    A polynomial
+
+    (1 - z^(-1))^N
+    (1 - z^(-1))^2
+    (1 - z^(-1)) * (1 - z^(-1))
+    1 - z^(-1) - z^(-1) + z^(-2)
+    1 - 2z^(-1) + z^(-2)
+    """
     a = [1, -2, 1]
-    (w, h) = freqz(b=b, a=a)
-    f = w * fs / (2 * np.pi)
-    h_db = 20 * np.log10(np.abs(h))
-    h_phase = np.angle(h) * 180 / np.pi
-    print(h_phase)
-    axs.set_title('Frequency Response')
-    axs.semilogx(f, h_db)
+    
+    (w, h) = freqz(b=b, a=a, fs=fs)
+    plt.plot(w, 20 * np.log10(abs(h)))
+    plt.title(f"wave_samp_per = {wave_samp_per}")
+    
     plt.show()
 
 
